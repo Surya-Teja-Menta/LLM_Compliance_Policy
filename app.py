a714f0cafe32d2427b66d96e3ea767405a6e9345
Surya Teja Menta <79039074+Surya-Teja-Menta@users.noreply.github.com>
2023-10-14 13:54:59 +0530

    Update Successfully

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ warnings.filterwarnings("ignore")
 
 try:
     openai.api_key = st.secrets['API_KEY']
-    #openai.api_key = "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"
 except Exception as e:
     st.write(e) 
 
